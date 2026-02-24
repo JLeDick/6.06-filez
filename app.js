43e0a8bf13aa5f958fc1629c493c03c3bf8cc8a5
Jason Dick <Jason.L.Dick@gmail.com>
2026-02-24 02:58:22 +0100

    Finished - 3am sry no other commits, this was rough

--- app.js
+++ app.js
@@ -1,3 +1,116 @@
 import express from "express";
+import db from "#db/client";
 const app = express();
 export default app;
+
+// body parse
+app.use(express.json());
+
+// get files
+app.get("/files", async (req, res, next) => {
+  try {
+    const { rows } = await db.query(
+      `
+      SELECT files.*, folders.name AS folder_name
+      FROM files
+      JOIN folders 
+      ON files.folder_id = folders.id
+      `,
+    );
+    res.send(rows);
+  } catch (e) {
+    next(e);
+  }
+});
+
+// get folders
+app.get("/folders", async (req, res, next) => {
+  try {
+    const { rows } = await db.query(
+      `
+    SELECT * FROM folders
+    `,
+    );
+    res.send(rows);
+  } catch (e) {
+    next(e);
+  }
+});
+
+// get folders by id
+app.get("/folders/:id", async (req, res, next) => {
+  try {
+    const { rows: folders } = await db.query(
+      `
+    SELECT * FROM folders
+    WHERE id = $1
+    `,
+      [req.params.id],
+    );
+
+    // 404 if no exist
+    if (!folders.length) {
+      return res.status(404).send({ message: "folder not found" });
+    }
+
+    const { rows: files } = await db.query(
+      `
+      SELECT * FROM files 
+      WHERE folder_id = $1
+      `,
+      [req.params.id],
+    );
+
+    const folder = folders[0];
+    folder.files = files;
+    res.send(folder);
+  } catch (e) {
+    next(e);
+  }
+});
+
+// update folder - this one was rough wtf - it's so ugly
+// I needed help on this. I thought I had this right like 12 times
+app.post("/folders/:id/files", async (req, res, next) => {
+  try {
+    // Check if folder exists
+    const { rows: folders } = await db.query(
+      `
+      SELECT * FROM folders 
+      WHERE id = $1
+      `,
+      [req.params.id],
+    );
+
+    if (!folders.length) {
+      return res.status(404).send({ message: "folder not found" });
+    }
+
+    if (!req.body || Object.keys(req.body).length === 0) {
+      return res.status(400).send({ message: "Missing name and size" });
+    }
+
+    if (!req.body.name || !req.body.size) {
+      return res.status(400).send({ message: "Missing required fields" });
+    }
+
+    const { rows } = await db.query(
+      `
+      INSERT INTO files (name, size, folder_id)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+      `,
+      [req.body.name, req.body.size, req.params.id],
+    );
+
+    res.status(201).send(rows[0]);
+  } catch (e) {
+    next(e);
+  }
+});
+
+// error handler => asyncHandlers make way more sense
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send({ mesage: err.message || "Internal Server Error" });
+});
--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,51 @@
 import db from "#db/client";
+import { faker } from "@faker-js/faker";
+
+let folderCount = 3;
+let filePerFolderCount = 5;
 
 await db.connect();
-await seed();
+await seed(folderCount, filePerFolderCount);
 await db.end();
 console.log("🌱 Database seeded.");
 
-async function seed() {
-  // TODO
+async function seed(folderCount, filePerFolderCount) {
+  for (let i = 0; i < folderCount; i++) {
+    const name = faker.music.genre();
+    const {
+      rows: [createdFolder],
+    } = await db.query(
+      `
+      INSERT INTO folders (name)
+      VALUES ($1)
+      RETURNING *
+      `,
+      [name],
+    );
+
+    for (let j = 0; j < filePerFolderCount; j++) {
+      const name = faker.music.songName();
+      const size = faker.number.int({ min: 9, max: 105 });
+      const {
+        rows: [createdFile],
+      } = await db.query(
+        `
+        INSERT INTO files (name, size, folder_id)
+        VALUES ($1, $2, $3)
+        RETURNING *
+        `,
+        [name, size, createdFolder.id],
+      );
+    }
+  }
 }
+
+/*
+From the documentation it looks like, if I used Promise.all instead
+of awaiting each one of the queries, then it would be faster because
+all of the queries would run sequentially rather than individually,
+but I don't really care for this project. Just writing this to remember.
+
+Also, as far as I can tell I don't need to destructure the second db.query
+because I don't do anything with the result. I'm going to remove it.
+*/
